dcb827d7d392f693750338735d97aee22f4257fc
Эльнара Габдрахманова <elnaradin@gmail.com>
2023-01-27 02:16:55 +0500

    spring-context

---
+++ spring-context-example/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        RoboFactory factory = (RoboFactory) context.getBean("factory");
+        factory.runProduction();
+        System.out.println("total beans count: " + context.getBeanDefinitionCount());
+        System.out.println("beans names: " + Arrays.toString(context.getBeanDefinitionNames()));
+    }
+}
